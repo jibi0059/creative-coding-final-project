3737a4a9a8bbf7b3c056ddced347657fe1fcbaf6
ywan0820 <wanghenshui02@gmail.com>
2026-06-05 18:58:39 +1000

    Update perlin-mechanic.js

--- p5_project/perlin-mechanic.js
+++ p5_project/perlin-mechanic.js
@@ -1,3 +1,9 @@
+const seaTopRatio = 0.5;
+//The sea accounts for half.
+
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 360, 100, 100, 100);
@@ -6,4 +12,10 @@ function setup() {
 
 function draw() {
   background(0, 0, 100);
+
+  let seaTop = height * seaTopRatio;
+
+  fill(200, 80, 45);
+  rect(0, seaTop, width, height - seaTop);
+  //The extent of the sea.
 }
