13c8d0fc43527413c2593e470cac9dc6eb3cea24
ywan0820 <wanghenshui02@gmail.com>
2026-06-06 01:52:22 +1000

    ocean area

--- p5_project/perlin-mechanic.js
+++ p5_project/perlin-mechanic.js
@@ -18,8 +18,29 @@ function setupPerlinMechanic() {
 
 
 function drawPerlinMechanic() {
+  const seaTop = height * PERLIN_SEA_TOP_RATIO;
+// half of the canva is the sea
+
+  perlinLayer.noStroke();
+  perlinLayer.fill(20, 80, 140, 35);
+  perlinLayer.rect(0, seaTop, width, height - seaTop);
+  //extent of the sea
+  
   image(perlinLayer, 0, 0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function resizePerlinMechanic() {
