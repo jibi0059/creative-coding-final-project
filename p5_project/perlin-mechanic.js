5bfb30d5fa32f82e7bae4b3913a71bb3efffb26d
ywan0820 <wanghenshui02@gmail.com>
2026-06-05 19:25:16 +1000

    layer for Perlin

--- p5_project/perlin-mechanic.js
+++ p5_project/perlin-mechanic.js
@@ -1,5 +1,28 @@
-function setupPerlinMechanic() {}
 
-function drawPerlinMechanic() {}
 
-function resizePerlinMechanic() {}
+
+let perlinLayer;
+
+
+
+function setupPerlinMechanic() {
+  perlinLayer = createGraphics(width, height);
+  perlinLayer.noStroke();
+
+
+
+}
+
+
+
+
+
+function drawPerlinMechanic() {
+  image(perlinLayer, 0, 0);
+}
+
+
+function resizePerlinMechanic() {
+  perlinLayer = createGraphics(width, height);
+  perlinLayer.noStroke();
+}
