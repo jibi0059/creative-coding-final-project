4cb67b928905c7f0d002ef287d5f333e94c6f0e8
ywan0820 <wanghenshui02@gmail.com>
2026-06-06 14:40:08 +1000

    every painters get life

--- p5_project/perlin-mechanic.js
+++ p5_project/perlin-mechanic.js
@@ -51,16 +51,29 @@ function resetOcean() {
 
 class OceanPainter {
   constructor() {
+    this.reset();
+  }
+  
+  
+  reset(){
     const seaTop = height * SEA_TOP_RATIO;
 
     this.x = random(width);
     this.y = random(seaTop, height);
     this.size = random(8, 28);
     this.speed = random(0.7, 2.2);
+    this.life = random(90, 180);
+    this.age = 0;
+    //limite life to kill some painters
     this.seed = random(1000);
     //aim to move to different routes
   }
+
+
+
   move() {
+
+    const seaTop = height * SEA_TOP_RATIO;
     const noiseValue = noise(
       this.x * 0.004,
       this.y * 0.008,
@@ -68,11 +81,27 @@ class OceanPainter {
     );
     const angle = map(noiseValue, 0, 1, -PI * 0.15, PI * 0.15);
       this.x += cos(angle) * this.speed;
-      this.y += sin(angle) * this.speed * 0.5;
-    //move up and down randomly, do not hgher than seatop
+      this.y += sin(angle) * this.speed;
+    //move up and down randomly
     
+    this.age++;
+
+    if (
+      this.x > width + 40 || 
+      this.x < -40 ||
+      this.y > seaTop ||
+      this.y > height ||
+      this.age > this.life
+    
+    ) {
+      this.reset();
+      //reycycle
   }
-   paint() {
+}
+   
+  
+
+  paint() {
     perlinLayer.fill(80, 160, 220, 80);
     perlinLayer.ellipse(this.x, this.y, this.size * 2.5, this.size * 0.18);
 
